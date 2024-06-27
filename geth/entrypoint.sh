c7879ce989a9be0c73fcc1565cd9face281c680d
dappnodedev <diego@dappnode.io>
2024-06-27 16:59:37 +0200

    Fix extra flags

--- geth/entrypoint.sh
+++ geth/entrypoint.sh
@@ -2,12 +2,21 @@
 
 handle_network() {
   case "$NETWORK" in
-  "holesky") set_network_specific_config "--holesky" ;;
+  "holesky")
+    export GETH_HOLESKY=true
+    set_network_specific_config
+    ;;
   "lukso")
     set_network_specific_config "--networkid 42 --miner.gasprice 1000000000 --miner.gaslimit 42000000 --bootnodes enode://c2bb19ce658cfdf1fecb45da599ee6c7bf36e5292efb3fb61303a0b2cd07f96c20ac9b376a464d687ac456675a2e4a44aec39a0509bcb4b6d8221eedec25aca2@34.147.73.193:30303,enode://276f14e4049840a0f5aa5e568b772ab6639251149a52ba244647277175b83f47b135f3b3d8d846cf81a8e681684e37e9fc10ec205a9841d3ae219aa08aa9717b@34.32.192.211:30303 --maxpeers 50"
     ;;
-  "mainnet") set_network_specific_config "--mainnet" ;;
-  "sepolia") set_network_specific_config "--sepolia" ;;
+  "mainnet")
+    export GETH_MAINNET=true
+    set_network_specific_config
+    ;;
+  "sepolia")
+    export GETH_SEPOLIA=true
+    set_network_specific_config
+    ;;
   *)
     echo "Unsupported network: $NETWORK"
     exit 1
@@ -16,7 +25,11 @@ handle_network() {
 }
 
 set_network_specific_config() {
-  EXTRA_FLAGS="$1 $EXTRA_FLAGS"
+  config_flags=$1
+
+  if [ -n "$config_flags" ]; then
+    EXTRA_FLAGS="$config_flags $EXTRA_FLAGS"
+  fi
 
   echo "[INFO - entrypoint] Initializing $NETWORK specific config for client"
 
@@ -53,7 +66,7 @@ set_jwt_path() {
 }
 
 post_jwt_to_dappmanager() {
-  echo "[INFO - entrypoint] Posting JWT to DAppManager"
+  echo "[INFO - entrypoint] Posting JWT to Dappmanager"
   JWT=$(cat "${JWT_PATH}")
 
   curl -X POST "http://my.dappnode/data-send?key=jwt&data=${JWT}" || {
@@ -62,12 +75,12 @@ post_jwt_to_dappmanager() {
 }
 
 run_client() {
+  # shellcheck disable=SC2086
   exec geth \
     --datadir "${DATA_DIR}" \
     --syncmode "${SYNCMODE:-snap}" \
     --port "${P2P_PORT}" \
-    --authrpc.jwtsecret "${JWT_PATH}" \
-    "${EXTRA_FLAGS}"
+    --authrpc.jwtsecret "${JWT_PATH}" ${EXTRA_FLAGS}
 }
 
 handle_network
