48dffe18a639ae6301cb476ba00ce04db90f94da
dappnodedev <diego@dappnode.io>
2024-07-22 16:52:29 +0200

    Use latest staker package scripts version

--- geth/entrypoint.sh
+++ geth/entrypoint.sh
@@ -1,43 +1,36 @@
 #!/bin/sh
 
-SUPPORTED_NETWORKS="holesky mainnet"
+SUPPORTED_NETWORKS="holesky mainnet lukso sepolia"
 
 # shellcheck disable=SC1091
 . /etc/profile
 
-handle_network() {
-  case "$NETWORK" in
-  "holesky")
-    set_execution_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}"
-    export GETH_HOLESKY=true
-    ;;
-  "lukso")
-    set_execution_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "--networkid 42 --miner.gasprice 1000000000 --miner.gaslimit 42000000 --bootnodes enode://c2bb19ce658cfdf1fecb45da599ee6c7bf36e5292efb3fb61303a0b2cd07f96c20ac9b376a464d687ac456675a2e4a44aec39a0509bcb4b6d8221eedec25aca2@34.147.73.193:30303,enode://276f14e4049840a0f5aa5e568b772ab6639251149a52ba244647277175b83f47b135f3b3d8d846cf81a8e681684e37e9fc10ec205a9841d3ae219aa08aa9717b@34.32.192.211:30303 --maxpeers 50"
-    geth --datadir="$DATA_DIR" init /config/genesis.json
-    # TODO: Check if this works properly
-    ;;
-  "mainnet")
-    set_execution_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}"
-    export GETH_MAINNET=true
-    ;;
-  "sepolia")
-    set_execution_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}"
-    export GETH_SEPOLIA=true
-    export JWT_PATH="/security/sepolia/jwtsecret.hex"
-    ;;
-  esac
+JWT_PATH=$(get_jwt_path "${NETWORK}" "${SUPPORTED_NETWORKS}" "${SECURITY_PATH}")
 
-}
+case "$NETWORK" in
+"holesky")
+  NETWORK_FLAGS="--holesky"
+  ;;
+"lukso")
+  NETWORK_FLAGS="--networkid 42 --miner.gasprice 1000000000 --miner.gaslimit 42000000 --bootnodes enode://c2bb19ce658cfdf1fecb45da599ee6c7bf36e5292efb3fb61303a0b2cd07f96c20ac9b376a464d687ac456675a2e4a44aec39a0509bcb4b6d8221eedec25aca2@34.147.73.193:30303,enode://276f14e4049840a0f5aa5e568b772ab6639251149a52ba244647277175b83f47b135f3b3d8d846cf81a8e681684e37e9fc10ec205a9841d3ae219aa08aa9717b@34.32.192.211:30303 --maxpeers 50"
+  geth --datadir="$DATA_DIR" init /config/genesis.json
+  ;;
+"mainnet")
+  NETWORK_FLAGS="--mainnet"
+  ;;
+"sepolia")
+  NETWORK_FLAGS="--sepolia"
+  JWT_PATH="/security/sepolia/jwtsecret.hex"
+  ;;
+esac
 
-run_client() {
-  # shellcheck disable=SC2086
-  exec geth \
-    --datadir "${DATA_DIR}" \
-    --syncmode "${SYNCMODE:-snap}" \
-    --port "${P2P_PORT}" \
-    --authrpc.jwtsecret "${JWT_PATH}" ${EXTRA_OPTS}
-}
+echo "[INFO - entrypoint] Starting geth with network flags: $NETWORK_FLAGS"
 
-handle_network
-post_jwt_to_dappmanager
-run_client
+post_jwt_to_dappmanager "${JWT_PATH}"
+
+# shellcheck disable=SC2086
+exec geth \
+  --datadir "${DATA_DIR}" \
+  --syncmode "${SYNCMODE:-snap}" \
+  --port "${P2P_PORT}" \
+  --authrpc.jwtsecret "${JWT_PATH}" ${NETWORK_FLAGS} ${EXTRA_OPTS}
